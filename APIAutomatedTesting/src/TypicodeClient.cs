05990c05ce747ae1b618fec874c169cc6209ac4b
Zhan Kariahin <Zhan.Kariahin@gmail.com>
2026-04-22 16:46:50 +0300

    Added task1, task2, task3 and Logic around Typicode

---
+++ APIAutomatedTesting/src/TypicodeClient.cs
@@ -0,0 +1,50 @@
+﻿using APITestAutomation.Tests.src.Models;
+using RestSharp;
+using RestSharp.Serializers.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace APITestAutomation.Tests.src
+{
+    public class TypicodeClient
+    {
+        private readonly IRestClient _client;
+
+        public TypicodeClient(string endpoint)
+        {
+            var serializerOptions = new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull };
+            this._client = new RestClient(options: new() { BaseUrl = new Uri(endpoint) }, 
+                configureSerialization: s => s.UseSystemTextJson(serializerOptions));
+        }
+
+        public async Task<List<Typicode>> GetTypicodesAsync()
+        {
+            var request = new RestRequest($"/users", Method.Get);
+            return await this._client.GetAsync<List<Typicode>>(request) ?? new List<Typicode>();
+        }
+
+        public async Task<RestResponse<List<Typicode>>> GetTypicodesResponseAsync()
+        {
+            var request = new RestRequest($"/users", Method.Get);
+            return await this._client.ExecuteAsync<List<Typicode>>(request);
+        }
+
+        public async Task<Typicode> CreateTypicodeAsync(Typicode user)
+        {
+            var request = new RestRequest($"/users", Method.Post).AddJsonBody(user);
+            return await this._client.PostAsync<Typicode>(request) ?? new Typicode();
+        }
+
+        public async Task<RestResponse> UpdateTypicodeAsync(string id, Typicode user)
+        {
+            var request = new RestRequest($"/users/{id}", Method.Put).AddJsonBody(user);
+            return await this._client.PutAsync(request);
+        }
+
+        public async Task<RestResponse> DeleteTypicodeAsync(string id)
+        {
+            var request = new RestRequest($"/users/{id}", Method.Delete);
+            return await this._client.DeleteAsync(request);
+        }
+    }
+}
---
+++ APIAutomatedTesting/src/TypicodeFixture.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System.Net;
+
+namespace APITestAutomation.Tests.src
+{
+    public class TypicodeFixture
+    {
+        private readonly TypicodeClient typicodeClient = new($"https://jsonplaceholder.typicode.com/users");
+
+        [Test]
+        public async Task ValidateThatListOfUsersCanBeReceivedAndFieldsAreNotNullOrEmpty()
+        {
+            var response = await this.typicodeClient.GetTypicodesResponseAsync();
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            Assert.That(response.Data, Is.Not.Null);
+            Assert.That(response.Data!.Count, Is.GreaterThan(0));
+        }
+
+        //Validate response header for a list of users
+        [Test]
+        public async Task ValidateResponseHeaderForListOfUsers()
+        {
+            var response = await this.typicodeClient.GetTypicodesResponseAsync();
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            Assert.That(response.Headers, Is.Not.Null.And.Not.Empty);
+
+            var contentTypeHeader = response.Headers!
+                .FirstOrDefault(contentType => contentType.Name.Equals("Content-Type", StringComparison.OrdinalIgnoreCase));
+
+            Assert.That(contentTypeHeader, Is.Not.Null);
+            Assert.That(contentTypeHeader!.Value.ToString(), Is.EqualTo("application/json; charset=utf-8"));
+        }
+
+        [Test]
+        public async Task ValidateResponseContentForListOfUsers()
+        {
+            var response = await this.typicodeClient.GetTypicodesResponseAsync();
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            Assert.That(response.Data, Is.Not.Null);
+            Assert.That(response.Data!.Count, Is.EqualTo(10));
+
+            var ids = response.Data!.Select(data => data.Id).ToList();
+            Assert.That(ids.Distinct().Count(), Is.EqualTo(ids.Count));
+
+            Assert.Multiple(() =>
+            {
+                foreach (var user in response.Data)
+                {
+                    Assert.That(user.Name, Is.Not.Null.And.Not.Empty);
+                    Assert.That(user.Username, Is.Not.Null.And.Not.Empty);
+                    Assert.That(user.Company, Is.Not.Null);
+                    Assert.That(user.Company.Name, Is.Not.Null.And.Not.Empty);
+                }
+            });
+        }
+    }
+}
